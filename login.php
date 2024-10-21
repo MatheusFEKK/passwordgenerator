6b7d6c78ad88a0134390640e6e5f4142167d7112
Matheus Felipe Lazarini Braghin <matheusfelipelazarini4@gmail.com>
2024-10-21 13:46:17 -0300

    Tela de Login criada

--- login.php
+++ login.php
@@ -1,6 +1,17 @@
 <?php include 'navbar.php' ?>
 
-
+<div class="container">
+    <div class="box">
+        <h3 style="color: white; margin-bottom:2rem;">CADASTRO</h3>
+        <input class="input-user shadow" type="text" name="nome" placeholder="NOME">
+        <input class="input-user shadow" type="text" name="email" placeholder="E-MAIL">
+        <input class="input-user shadow" type="text" name="senha" placeholder="SENHA">
+        <div class="buttons">
+            <button class="user-btn shadow">CADASTRAR</button><br>
+            <button class="user-btn shadow">JÁ TEM CONTA?</button>
+        </div>
+    </div>
+</div>
 
 
 
